ea660d684c5aa5240ee204230fc5be51f5f9b094
baihaqiramadhani-11 <baihakiramadhani66@gmail.com>
2026-09-22 10:51:43 +0700

    first commit

---
+++ ControlStructure1.py
@@ -0,0 +1,12 @@
+performance = int(input("enter student performance: "))
+
+if performance >= 90:
+    print ("excellent performance")
+elif performance >= 80:
+    print ("very good performance")
+elif performance >= 70:
+    print ("good performance") 
+elif performance >= 60:
+    print ("average performance") 
+else:
+    print ("poor performance")
